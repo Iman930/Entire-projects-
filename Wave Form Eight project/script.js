4deff781fc8061e0a63a62434d3293c0613d7c1a
imankhalidbl-cloud <imankhalidbl@gmail.com>
2026-07-22 17:57:03 +0500

    Added Wave Form project

---
+++ Wave Form Eight project/script.js
@@ -0,0 +1,8 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)//When I write this line of code the letter pop up 
+        .join('')
+})
